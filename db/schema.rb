a2718f4828948b4ac35ad497ef151c3061f03862
anchon <s.y24-soccer-0406@i.softbank.jp>
2020-05-02 12:29:57 +0000

    編集済み

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2020_05_02_091113) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-  end
-
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-  end
 
 end
