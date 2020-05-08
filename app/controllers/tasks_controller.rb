44d51c5173f447f016bdb097bffc43ba3f972036
anchon <s.y24-soccer-0406@i.softbank.jp>
2020-05-08 17:39:04 +0000

    ログイン機能実装　修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -22,6 +23,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @microposts = current_user.microposts
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -43,7 +45,6 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = "Taskは正常に削除されました"
     redirect_to tasks_url
   end
@@ -54,10 +55,14 @@ class TasksController < ApplicationController
   end
   
   
-
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to login_path
+    end
+  end
 end
