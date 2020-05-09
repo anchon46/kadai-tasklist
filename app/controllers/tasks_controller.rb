61dc7a23808a62fbb54f71d2f20281e770893bec
anchon <s.y24-soccer-0406@i.softbank.jp>
2020-05-09 09:12:47 +0000

    ログイン機能追加と一対多をタスク管理アプリに追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  
   def index
     @tasks = current_user.tasks
   end
@@ -50,10 +49,6 @@ class TasksController < ApplicationController
   end
   
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   
   def task_params
     params.require(:task).permit(:content, :status)
@@ -62,7 +57,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to login_path
+      redirect_to root_path
     end
   end
 end
